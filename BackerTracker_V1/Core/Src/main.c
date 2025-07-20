68f54c46b8303f4f909f4ed67a1ba5258458898a
SabooostionG <74569821+SabooostionG@users.noreply.github.com>
2025-07-19 17:14:22 -0700

    Promdata all 7710 error

--- BackerTracker_V1/Core/Src/main.c
+++ BackerTracker_V1/Core/Src/main.c
@@ -61,8 +61,8 @@ uint8_t LEDstate = 0;
 uint32_t LEDtimer = 500;
 
 // Baro //
-#define BARO_SAMPLERATE_MS 450
-uint32_t barotimer = 50;
+#define BARO_SAMPLERATE_MS 750
+uint32_t barotimer = 500;
 
 uint16_t baroState = 0;
 uint32_t baroPressure=0;
@@ -131,11 +131,11 @@ int main(void)
 
   // Disable LoRa
   //HAL_GPIO_WritePin(GPIOB, GPIO_PIN_7, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
+  //HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_SET);
 
   // Baro CS pin -> A15
-  if(MS5607_Init(&hspi1, GPIOA,  GPIO_PIN_15)){
-	  baroState = 1;
+  baroState = MS5607_Init(&hspi1, GPIOA,  GPIO_PIN_15);
+  if(baroState == 1){
 	  HAL_Delay(250);
 
 	  // Three short beeps
@@ -153,7 +153,7 @@ int main(void)
 	  HAL_Delay(100);
 
   } else {
-	  baroState = 0;
+	  //baroState = 0;
 	  HAL_Delay(250);
 
 	  // 1 long beeps
@@ -186,8 +186,13 @@ int main(void)
 //			  baroPressure = MS5607GetPressurePa();
 //			  baroTemp = MS5607GetTemperatureC();
 //			  barotimer = HAL_GetTick();
+//
+//			  enableCSB();
+//			  HAL_SPI_Transmit(&hspi1, 0x1E, 1, 10);
+//			  disableCSB();
 //		  }
 //	  }
+
 
 //	  if( ((HAL_GetTick() - barotimer) >= BARO_SAMPLERATE_MS)){
 //		  MS5607Update();
@@ -197,55 +202,58 @@ int main(void)
 //	  }
 
 
-	  if( ((HAL_GetTick() - barotimer) >= BARO_SAMPLERATE_MS)){
-//		  MS5607Update();
-//		  baroPressure = MS5607GetPressurePa();
-//		  baroTemp = MS5607GetTemperatureC();
-		  /*Sensor reply data buffer*/
-		  uint8_t reply[3];
 
-		  enableCSB();
-		  /* Assemble the conversion command based on previously set OSR */
-		  HAL_SPI_Transmit(&hspi1, CONVERT_D1_COMMAND | 0x00, 1, 10);
-		  // Must wait >= 8.22ms after conversion
-		  HAL_Delay(10);
-		  disableCSB();
-
-		  /* Performs the reading of the 24 bits from the ADC */
-		  enableCSB();
-		  HAL_SPI_Transmit(&hspi1, READ_ADC_COMMAND, 1, 10);
-		  HAL_SPI_Receive(&hspi1, reply, 3, 10);
-		  disableCSB();
-
-		  /* Tranfer the 24bits read into a 32bit int */
-		  baroPressure = ((uint32_t) reply[0] << 16) | ((uint32_t) reply[1] << 8) | (uint32_t) reply[2];
-
-		  enableCSB();
-		  /* Assemble the conversion command based on previously set OSR */
-		  HAL_SPI_Transmit(&hspi1, CONVERT_D2_COMMAND | 0x00, 1, 10);
-		  // Must wait >= 8.22ms after conversion
-		  HAL_Delay(10);
-		  disableCSB();
-
-		  enableCSB();
-		  HAL_SPI_Transmit(&hspi1, READ_ADC_COMMAND, 1, 10);
-		  HAL_SPI_Receive(&hspi1, reply, 3, 10);
-		  disableCSB();
-
-		  /* Assemble the conversion command based on previously set OSR */
-		  baroTemperature = ((uint32_t) reply[0] << 16) | ((uint32_t) reply[1] << 8) | (uint32_t) reply[2];
-
-
-		  barotimer = HAL_GetTick();
-	  }
+//	  if( ((HAL_GetTick() - barotimer) >= BARO_SAMPLERATE_MS)){
+////		  MS5607Update();
+////		  baroPressure = MS5607GetPressurePa();
+////		  baroTemp = MS5607GetTemperatureC();
+//		  /*Sensor reply data buffer*/
+//		  uint8_t reply[3];
+//
+//		  enableCSB();
+//		  /* Assemble the conversion command based on previously set OSR */
+//		  HAL_SPI_Transmit(&hspi1, CONVERT_D1_COMMAND | 0x00, 1, 10);
+//		  // Must wait >= 8.22ms after conversion
+//		  HAL_Delay(10);
+//		  disableCSB();
+//
+//		  /* Performs the reading of the 24 bits from the ADC */
+//		  enableCSB();
+//		  HAL_SPI_Transmit(&hspi1, READ_ADC_COMMAND, 1, 10);
+//		  HAL_SPI_Receive(&hspi1, reply, 3, 10);
+//		  disableCSB();
+//
+//		  /* Tranfer the 24bits read into a 32bit int */
+//		  baroPressure = ((uint32_t) reply[0] << 16) | ((uint32_t) reply[1] << 8) | (uint32_t) reply[2];
+//
+//		  enableCSB();
+//		  /* Assemble the conversion command based on previously set OSR */
+//		  HAL_SPI_Transmit(&hspi1, CONVERT_D2_COMMAND | 0x00, 1, 10);
+//		  // Must wait >= 8.22ms after conversion
+//		  HAL_Delay(10);
+//		  disableCSB();
+//
+//		  enableCSB();
+//		  HAL_SPI_Transmit(&hspi1, READ_ADC_COMMAND, 1, 10);
+//		  HAL_SPI_Receive(&hspi1, reply, 3, 10);
+//		  disableCSB();
+//
+//		  /* Assemble the conversion command based on previously set OSR */
+//		  baroTemperature = ((uint32_t) reply[0] << 16) | ((uint32_t) reply[1] << 8) | (uint32_t) reply[2];
+//
+//
+//		  barotimer = HAL_GetTick();
+//	  }
 
 
 	  // Blink Blue LED
 	  if( (HAL_GetTick() - LEDtimer) >= 500){
 
+		  baroState = MS5607_Init(&hspi1, GPIOA,  GPIO_PIN_15);
+
 		  sprintf(logBuf,"Press: %d Pa | Temp: %0.1f C\r\n",(int)baroPressure,baroTemp);
 		  //sprintf(logBuf,"Pressure: %d [Pa]\r\n",(int)baroPressure);
-		  //CDC_Transmit_FS((uint8_t *) logBuf, strlen(logBuf));
+		  CDC_Transmit_FS((uint8_t *) logBuf, strlen(logBuf));
 
 		  LEDtimer = HAL_GetTick();
 
@@ -367,15 +375,15 @@ static void MX_SPI1_Init(void)
   hspi1.Init.Direction = SPI_DIRECTION_2LINES;
   hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
   hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
-  hspi1.Init.CLKPhase = SPI_PHASE_2EDGE;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
   hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
   hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
   hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
   hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
   hspi1.Init.CRCPolynomial = 7;
   hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
-  hspi1.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_ENABLE;
   if (HAL_SPI_Init(&hspi1) != HAL_OK)
   {
     Error_Handler();
